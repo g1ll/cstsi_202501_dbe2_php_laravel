316f56e327d3745794b04a872688cf2d80d308f1
g1ll <gillvelleda@gmail.com>
2025-04-04 12:48:37 -0300

    feat: criacao do controller UserController e rota "/users". O metodo index da classe UserController consulta a lista de usuarios da model User atraves do metodo all(). php artisan make:controller UserController

--- aula03/routes/web.php
+++ aula03/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,4 +16,4 @@ Route::get('/ola', function(){
 
 
 Route::get('/hola',[HomeController::class,'index']);
-
+Route::get('/users',[UserController::class,'index']);
