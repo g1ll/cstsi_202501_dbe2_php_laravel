ae28d915ec97d04936c85f95a32dd15c3ded9816
Prof. Gill <gillvelleda@gmail.com>
2025-04-04 15:51:58 -0300

    feat: alterado o template de users para mostrar os usuarios em uma tabela

--- aula03/resources/views/users/index.blade.php
+++ aula03/resources/views/users/index.blade.php
@@ -7,10 +7,28 @@
 </head>
 <body>
     <h1>Users:</h1>
-    <ul>
-    @foreach ($listUsers as $user)
-        <li>{{$user->name}}</li>
-    @endforeach
-    </ul>
+    <table>
+        @if ($listUsers->count() > 0)
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($listUsers as $user)
+                        <tr>
+                            <td>
+                                {{ $user->id }}
+                            </td>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>Produtos não encontrados! </p>
+        @endif
 </body>
 </html>
