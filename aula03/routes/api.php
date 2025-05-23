6a1eeda0efeb72485d8f0e178eb12f61a2188f92
Prof. Gill Gonzales <gillvelleda@gmail.com>
2025-05-23 15:16:28 -0300

    feat: configurações das rotas de login web e rotas de api protegidas.

--- aula03/routes/api.php
+++ aula03/routes/api.php
@@ -1,17 +1,30 @@
 <?php
 
 use App\Http\Controllers\Api\ProdutoController;
+use App\Http\Controllers\Api\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware("auth:sanctum");
 
 // Route::get('produtos',[ProdutoController::class,'index']);
 // Route::get('produtos/{produto}',[ProdutoController::class,'show']);
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('produtos', ProdutoController::class);
+
+    // Route::get('users',function(){
+    //     return User::all();
+    // })->middleware("force-json");
+
+    Route::get('users',function(){
+        return User::all();
+    });
+
+    Route::apiResource("users",UserController::class);
 });
+
 
--- aula03/routes/web.php
+++ aula03/routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -64,3 +68,11 @@ Route::controller(ProdutoController::class)->group(function () {
 
 Route::resource('fornecedores', FornecedorController::class)
     ->parameter('fornecedores', 'fornecedor');
+
+
+Route::post("/login",function(Request $request){
+    // return $request->all();
+    if(Auth::attempt(["email"=>$request->email,"password"=>$request->password]))
+        return new UserResource(User::where('email',$request->email)->get());
+    else return response()->json(["erro"=>"Dados inválidos!!"],401);
+});
