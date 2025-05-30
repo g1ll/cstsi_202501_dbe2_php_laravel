fd0b2d4c1056c932e81f4679666a222b3b6dd31e
g1ll <gillvelleda@gmail.com>
2025-05-30 11:52:59 -0300

    feat: atualização da model para aplicar abilidade is-admin

--- aula03/database/seeders/DatabaseSeeder.php
+++ aula03/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@test.dev',
+            'is_admin'=>true,
             'password'=> 'adminadmin'
         ]);
     }
