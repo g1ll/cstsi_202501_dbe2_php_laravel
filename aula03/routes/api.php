81013f4e41f7c0626dc82c11e2b3808073c4ca74
g1ll <gillvelleda@gmail.com>
2025-06-17 12:20:15 -0300

    refactor: movendo rota user para versao v1 da API

--- aula03/routes/api.php
+++ aula03/routes/api.php
@@ -7,14 +7,14 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware("auth:sanctum");
-
 // Route::get('produtos',[ProdutoController::class,'index']);
 // Route::get('produtos/{produto}',[ProdutoController::class,'show']);
 
 Route::prefix('v1')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+    return $request->user();
+    })->middleware("auth:sanctum");
 
     Route::apiResource('produtos', ProdutoController::class)
         ->middleware("auth:sanctum");
@@ -34,7 +34,7 @@ Route::prefix('v1')->group(function () {
         return User::all();
     });
 
-    Route::apiResource("users",UserController::class);
+    Route::apiResource("users",UserController::class)->middleware("auth:sanctum");
 
     Route::post('/login',[LoginController::class, 'login']);
 });
