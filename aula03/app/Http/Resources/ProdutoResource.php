3fb497e2b352e472ca9d3715282b77a2cfb3f696
g1ll <gillvelleda@gmail.com>
2025-06-27 11:08:32 -0300

    feat: monta os links das medias relacionadas ao produto.

--- aula03/app/Http/Resources/ProdutoResource.php
+++ aula03/app/Http/Resources/ProdutoResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ProdutoResource extends JsonResource
 {
@@ -14,6 +15,17 @@ class ProdutoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            ...parent::toArray($request),
+            'imagem_links' =>$this->when(
+                    $this->media,
+                    $this->media->map(
+                        fn($m)=>
+                            asset(
+                                Storage::url('produtos/'.$m->source)
+                                )
+                            )
+            )
+        ];
     }
 }
