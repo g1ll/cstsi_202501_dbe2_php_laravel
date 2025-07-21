921f724f7bca5c83e3050e202598810150ea69ba
g1ll <gillvelleda@gmail.com>
2025-07-21 12:49:22 -0300

    fix: detecta se o link da media possui http ou esta no estorage

--- aula03/app/Http/Resources/ProdutoResource.php
+++ aula03/app/Http/Resources/ProdutoResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProdutoResource extends JsonResource
 {
@@ -15,15 +16,15 @@ class ProdutoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // JsonResource::withoutWrapping();
         return [
             ...parent::toArray($request),
             'imagem_links' =>$this->when(
                     $this->media,
                     $this->media->map(
-                        fn($m)=>
-                            asset(
-                                Storage::url('produtos/'.$m->source)
-                                )
+                        fn($m)=>Str::contains($m->source,'http')
+                            ?$m->source
+                            :asset(Storage::url('produtos/'.$m->source))//http://localhost:8083/storage/produtos/nomeImagem.jpg
                             )
             )
         ];
